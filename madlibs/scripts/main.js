082f958cc96ec9adaf46de866a9d46d52384ac0a
anna hunter <annfmonty@gmail.com>
2016-03-07 13:53:08 -0600

    basic madlib

---
+++ madlibs/scripts/main.js
@@ -0,0 +1,16 @@
+//This will create a madlib story
+
+/*var noun = noun;
+var verb = verb;
+var adjective = adjective;
+var adverb = adverb;*/
+
+noun = prompt("Give me a noun");
+name = prompt("What is your best friend's name");
+verb = prompt("Give me a verb");
+adjective = prompt("Give me a adjective");
+adverb = prompt("Give me a adverb");
+
+console.log("Your "+adjective+" "+noun+" "+verb+" "+adverb+" with "+name+".");
+
+
